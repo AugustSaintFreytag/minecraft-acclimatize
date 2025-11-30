4138a6af64f6a361c2e688325419cf8d6f5d75cf
August Saint Freytag <me@augustfreytag.com>
2025-12-01 00:21:41 +0100

    Establish mixin to intercept vanilla rain sounds

---
+++ src/main/java/net/saint/acclimatize/mixin/SoundSystemMixin.java
@@ -0,0 +1,39 @@
+package net.saint.acclimatize.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundSystem;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.saint.acclimatize.Mod;
+import net.saint.acclimatize.sound.WeatherSoundInstance;
+
+@Mixin(SoundSystem.class)
+public abstract class SoundSystemMixin {
+
+	@Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+	private void acclimatize$disableVanillaRain(SoundInstance soundInstance, CallbackInfo callbackInfo) {
+		if (!Mod.CONFIG.enableRainSounds) {
+			return;
+		}
+
+		if (soundInstance instanceof WeatherSoundInstance) {
+			return;
+		}
+
+		if (soundInstance.getCategory() != SoundCategory.WEATHER) {
+			return;
+		}
+
+		var soundId = soundInstance.getId();
+
+		if (soundId.equals(SoundEvents.WEATHER_RAIN.getId()) || soundId.equals(SoundEvents.WEATHER_RAIN_ABOVE.getId())) {
+			callbackInfo.cancel();
+		}
+	}
+
+}
