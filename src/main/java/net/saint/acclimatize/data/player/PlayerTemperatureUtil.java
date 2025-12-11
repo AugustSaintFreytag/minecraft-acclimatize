470dcfd5d31cb8fbb079988a2511f56264a91579
August Saint Freytag <me@augustfreytag.com>
2025-12-11 02:58:08 +0100

    Tick space and wind managers per player

--- src/main/java/net/saint/acclimatize/data/player/PlayerTemperatureUtil.java
+++ src/main/java/net/saint/acclimatize/data/player/PlayerTemperatureUtil.java
@@ -30,7 +30,11 @@ public class PlayerTemperatureUtil {
 	public static void tickPlayerTemperature(ServerPlayerEntity player, ServerState serverState, PlayerState playerState) {
 		// Prerequisites
 
-		Mod.PLAYER_SPACE_MANAGER.getManagerForPlayer(player).tick(player);
+		var spaceManager = Mod.PLAYER_SPACE_MANAGER.getManagerForPlayer(player);
+		var windManager = Mod.PLAYER_WIND_MANAGER.getManagerForPlayer(player);
+
+		spaceManager.tick(player);
+		windManager.tick(serverState, player);
 
 		var bodyTemperature = playerState.bodyTemperature;
 		var isInInterior = Mod.PLAYER_SPACE_MANAGER.getManagerForPlayer(player).isPlayerInInterior();
