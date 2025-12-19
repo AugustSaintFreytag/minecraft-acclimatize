f71fbe540ba20ca220fac4da1f29a4376237ceeb
August Saint Freytag <me@augustfreytag.com>
2025-12-19 18:54:08 +0100

    Move GUI-related textures to "gui" directory

--- src/main/java/net/saint/acclimatize/hud/TemperatureHudUtil.java
+++ src/main/java/net/saint/acclimatize/hud/TemperatureHudUtil.java
@@ -305,11 +305,11 @@ public final class TemperatureHudUtil {
 	}
 
 	private static Identifier textureIdentifierForThermometer(String textureName) {
-		return new Identifier(Mod.MOD_ID, "textures/thermometer" + textureName);
+		return new Identifier(Mod.MOD_ID, "textures/gui/thermometer" + textureName);
 	}
 
 	private static Identifier textureIdentifier(THERMOMETER_STYLE style, String textureName) {
-		return new Identifier(Mod.MOD_ID, "textures/" + textureGroupForThermometerStyle(style) + "/" + textureName);
+		return new Identifier(Mod.MOD_ID, "textures/gui/" + textureGroupForThermometerStyle(style) + "/" + textureName);
 	}
 
 	private static String textureGroupForThermometerStyle(THERMOMETER_STYLE style) {
